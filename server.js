7abea4acb263371ada567d73c26e865f21465a34
Tathao Nguyen <tathaonguyen@gmail.com>
2022-09-06 10:13:42 +0700

    Modify the server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${FAVORITE_INSTRUCTOR} is the best!`);
     await sleep(5000);
   }
 }
